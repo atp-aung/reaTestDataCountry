2666ac94050f5871488afa1596b3715fd9fa2e0c
atp <atp.aung.5555@gmail.com>
2023-04-25 06:41:14 +0630

    a

--- src/App.js
+++ src/App.js
@@ -10,16 +10,14 @@ const App = () => {
   useEffect(() => {
     console.log("effect starts run");
 
-    if (filBox) {
-      console.log("fetching nations...");
-      axios
-        .get(`https://restcountries.com/v3.1/all`)
-        .then((response) => {
-          console.log(response);
-          setCounName(response.data);
-        })
-        .catch((err) => console.log("erro"));
-    }
+    console.log("fetching nations...");
+    axios
+      .get(`https://restcountries.com/v3.1/all`)
+      .then((response) => {
+        console.log(response);
+        setCounName(response.data);
+      })
+      .catch((err) => console.log("erro"));
   }, [filBox]);
 
   const handFiltChg = (e) => {
